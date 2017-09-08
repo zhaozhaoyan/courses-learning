34af0620b5dac94c7a45a0f87afaafc2be4a3ccc
zhaozhaoyan <2451819992@qq.com>
2017-09-08 11:19:34 +0800

    递归联系，根据视频里的例子

---
+++ C程序设计进阶/week3/单词翻转.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int i=0;
+char input[500];
+int recur()
+{
+  char c=input[i];
+  i++;
+  if(c==' ')
+  {
+    return 1;
+  }
+  if(c!=' '&&c!='\0')
+  {
+     recur();
+     cout<<c;
+  }
+  return 1;
+}
+int main()
+{
+    cin.getline(input,501);
+    if(recur()==1)  cout<<" ";
+    while(input[i]!='\0')
+    {
+      if(recur()==1) cout<<" ";
+    }
+    if(input[i]=='\0') cout<<endl;
+    return 0;
+}
