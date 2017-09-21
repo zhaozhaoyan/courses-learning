b49fae9f55900331f6b91af98634803dbfb63eec
zhaozhaoyan <2451819992@qq.com>
2017-09-21 15:35:24 +0800

    pair类模板

---
+++ 笔记程序示例/C++程序设计/Pair类模板.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+using namespace std;
+template<class T1,class T2>
+class Pair{
+public:
+  T1 key;
+  T2 value;
+  Pair(T1 k,T2 v):key(k),value(v){};
+  bool operator<(const Pair<T1,T2>&p)const;
+};
+template<class T1,class T2>
+bool Pair<T1,T2>::operator<(const Pair<T1,T2> & p)const
+{
+  return key < p.key;
+}
+//Pair模板的使用
+int main()
+{
+  Pair<string,int>student("Tom",19);
+  //实例化出一个类<string,int>
+  cout<<student.key<<" "<<student.value<<endl;
+  return 0;
+}
