b9a3b64ea5d7faf2bc5a5db24ebe91f95e1950cf
zhaozhaoyan <2451819992@qq.com>
2017-09-21 11:20:24 +0800

    文件复制操作

---
+++ 笔记程序示例/C++程序设计/文件复制操作程序.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main(int argc, char *argv[]) {
+  if(argc!=3){
+    cout<<"file name missing!"<<endl;
+    return 0;
+  }
+  ifstream inFile(argv[1],ios::binary|ios::in);//打开文件用于读
+  if(!inFile){
+    cout<<"source file open error."<<endl;
+    return 0;
+  }
+  ofstream outFile(argv[2],ios::binary|ios::out);//打开文件用于写
+  if(!outFile){
+    cout<<"new file open error."<<endl;
+    inFile.close();//打开文件一定要关闭
+    return 0;
+  }
+  char c;
+  while(inFile.get(c))//每次读取一个字符
+     outFile.put(c);//每次写入一个字符
+  outFile.close();
+  inFile.close();
+  return 0;
+}
