8cf0a52412cfac954224223b59ee0858b3e161ac
zhaozhaoyan <2451819992@qq.com>
2017-09-21 09:37:05 +0800

    使用多态时的注意事项

---
+++ 笔记程序示例/C++程序设计/在成员函数中调用虚函数.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+class Base{
+public:
+  void fun1(){this->fun2();}//this是基类指针
+  virtual void fun2(){
+    cout<<"Base::fun2()"<<endl;
+  }
+};
+class Derived:public Base{
+public:
+  virtual void fun2(){
+    cout<<"Derived:fun2()"<<endl;
+  }
+};
+int main()
+{
+  Derived d;
+  Base * pBase=&d;
+  pBase->fun1();
+  return 0;
+}
