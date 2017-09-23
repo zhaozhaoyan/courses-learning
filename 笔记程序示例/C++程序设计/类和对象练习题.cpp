9bc482392206414900ed7f6d4f4f908038301f6d
zhaozhaoyan <2451819992@qq.com>
2017-09-23 10:51:39 +0800

    类和对象练习题

---
+++ 笔记程序示例/C++程序设计/类和对象练习题.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+/**
+  * 定义类：Student
+  * 数据成员：名字、年龄
+  */
+  class Student
+{
+public:
+    // 定义数据成员名字 m_strName 和年龄 m_iAge
+    string m_strName;
+    int m_iAge;
+};
+
+int main()
+{
+    // 实例化一个Student对象stu
+    Student stu;
+    // 设置对象的数据成员
+    stu.m_strName = "慕课网";
+    stu.m_iAge = 2;
+
+    // 通过cout打印stu对象的数据成员
+    cout << stu.m_strName<< " " <<stu.m_iAge<< endl;
+    return 0;
+}
